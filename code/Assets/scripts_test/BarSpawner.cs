d14979925091ec85b20bcc37ab02e02617aba6e8
Zaptopod4001 <32018324+Zaptopod4001@users.noreply.github.com>
2020-06-01 13:59:32 +0300

    Update BarSpawner.cs

--- code/Assets/scripts_test/BarSpawner.cs
+++ code/Assets/scripts_test/BarSpawner.cs
@@ -6,6 +6,13 @@ using UnityEngine.UI;
 
 namespace EnergyBar
 {
+    // Copyright Sami S.
+
+    // use of any kind without a written permission 
+    // from the author is not allowed.
+
+    // DO NOT:
+    // Fork, clone, copy or use in any shape or form.
 
     // Demo class
     // How to spawn a new energy bar
