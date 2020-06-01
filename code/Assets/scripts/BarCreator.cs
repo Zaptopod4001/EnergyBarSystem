63a939d968976af101487792bdc6bba7c6d821cf
Zaptopod4001 <32018324+Zaptopod4001@users.noreply.github.com>
2020-06-01 13:58:57 +0300

    Update BarCreator.cs

--- code/Assets/scripts/BarCreator.cs
+++ code/Assets/scripts/BarCreator.cs
@@ -5,6 +5,13 @@ using UnityEngine.EventSystems;
 
 namespace EnergyBar
 {
+    // Copyright Sami S.
+
+    // use of any kind without a written permission 
+    // from the author is not allowed.
+
+    // DO NOT:
+    // Fork, clone, copy or use in any shape or form.
 
     // Factory that spits out energy bars
     // and parents them to assigned canvas
